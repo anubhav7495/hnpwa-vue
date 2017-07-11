323cb638f69ad0f782cb32506f67f7f0ae4fc7f3
anubhav7495 <anubhav.worklinux@gmail.com>
2017-07-11 23:43:27 +0530

    Added route based code splitting

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
-import CommentsList from '@/components/CommentsList';
 
 Vue.use(Router);
+
+// route-level code splitting
+const Home = () => import('../components/Home.vue');
+const CommentsList = () => import('../components/CommentsList.vue');
 
 export default new Router({
   routes: [
