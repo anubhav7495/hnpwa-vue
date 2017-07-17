6ff4962d8622fbb4c6ea8b021789e6feac2ac5e3
anubhav7495 <anubhav.worklinux@gmail.com>
2017-07-17 20:24:38 +0530

    Moved api-data-preloader before preload scripts

--- build/api-data-preload.js
+++ build/api-data-preload.js
@@ -1,8 +1,8 @@
 const q = window.location.hash ? window.location.hash.slice(1) : '/news';
 fetch('https://node-hnapi.herokuapp.com' + q)
-  .then((res) => {
-    if(res.ok) return res.json();
+  .then((r) => {
+    if (r.ok) return r.json();
     throw new Error('api data preload failed');
   })
-  .then((res) => {sessionStorage.setItem(q, JSON.stringify(res))})
+  .then((r) => {sessionStorage.setItem(q, JSON.stringify(r))})
   .catch(e => console.error(e.message));
